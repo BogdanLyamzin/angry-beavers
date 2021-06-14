2d1a5feba671f74cf27abc91f6c0459ec835ac9c
Akbar <k.akbar1101@gmail.com>
2021-06-14 12:53:24 +0300

    structure

---
+++ src/client/components/sidebar/components/SidebarElems.js
@@ -0,0 +1,30 @@
+import {v4} from 'uuid'
+
+const SidebarElems = [
+    
+    {
+        page: "Main",
+        to: "/",
+        id: `${v4()}`
+    },
+    {
+        page: "Work-shift",
+        to: "/work-shift",
+        id: `${v4()}`
+    },
+    {
+        page: "Work-place",
+        to: "/work-place",
+        id: `${v4()}`
+    },
+    {
+        page: "Staff",
+        to: "/staff",
+        id: `${v4()}`
+    },
+        {
+        page: "Working-month",
+        to: "/working-month",
+        id: `${v4()}`
+    }
+]
